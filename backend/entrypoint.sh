c1fcf6b83c5ebcc5894ecc6eb30d7a82ee74893f
oasisc1208@icloud.com <oasisc1208@icloud.com>
2023-06-14 13:23:08 +0000

    entrypoint.sh 환경 변수 선언 및 Migration 명령어 수정

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+conf="--settings=config.settings.dev-setting"
+
 echo "===== Create Specification ====="
 
-python manage.py makemigrations auth_app
-python manage.py makemigrations platform_app
+python manage.py makemigrations auth_app "$conf"
+python manage.py makemigrations platform_app "$conf"
 
 echo "===== Create Database By Specification ====="
 
-python manage.py migrate auth_app
-python manage.py migrate platform_app
+python manage.py migrate auth_app "$conf"
+python manage.py migrate platform_app "$conf"
 
 echo "=====  AppServer Start ====="
-python manage.py runserver 0.0.0.0:80 --settings=config.settings.dev-setting   # 개발용 테스트 서버 기동 (Runserver)
+python manage.py runserver 0.0.0.0:80 "$conf"  # 개발용 테스트 서버 기동 (Runserver)
 
 # gunicorn --workers=4 --bind 0.0.0.0:80 project_core.wsgi:application \  
 #--access-logfile history/access.log --error-logfile history/error.log  # 프로덕션 버전 서버 기동 (Gunicorn)
