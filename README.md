4ca74e24f7f7697aac0fce8ad04fed470e1ac8c7
oasisc1208@icloud.com <oasisc1208@icloud.com>
2023-06-15 04:24:10 +0000

    README.md Update && 과제 테스트 시작

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
-
-
 ### 버전 정보
+
 - Python : 3.9
 - Django : 4.2.1
-- Postgres : 13 
+- Postgres : 13
 
+### 변경 및 도입 정보
 
-### 변경 및 도입 정보 
-
-- psycopg2가 desperated가 될 수 있다는 문장이 언급되므로  psycopg[binary]에서 3버전을 설치한다.
+- psycopg2가 desperated가 될 수 있다는 문장이 언급되므로 psycopg[binary]에서 3버전을 설치한다.
 - psycopg2 vs psycopg3의 성능 차이는 아래 링크를 참고하자 (3 초기 버전의 벤치마크라서 성능 최적화가 덜 되었다는 말이 있으나 3.1 버전에서 성능 최적화가 되었을 것이라고 본다.)
 - URL : https://www.spherex.dev/psycopg-2-vs-psycopg3/
 
 - django-environ 도입
-    - django setting의 환경 변수 관리를 위한 env를 도입
+  - django setting의 환경 변수 관리를 위한 env를 도입
+
+### 제약 사항
+
+- Python 3.9 이상
+- django 3.2 이상
+- DRF 3.0 이상
+
+- Django 및 DRF를 이용해서 구현할 것
+- Database는 자유롭게 이용 가능
+- Restful API로 구현할 것
+- 테스크 코드를 작성할 것
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,30 +23,41 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 urlpatterns = [
-    path('api', include('auth_app.urls')),
-    path('api', include('platform_app.urls')),    
+    path("api", include("auth_app.urls")),
+    path("api", include("platform_app.urls")),
 ]
 
 
 ## Swagger 설정 정보 (drf-yasg)
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Simple Project3 API",
-      default_version='v0.1',
-      description="simple-project3-api-document",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="oasisc1208@icloud.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[AllowAny],
+    openapi.Info(
+        title="Simple Project3 API",
+        default_version="v0.1",
+        description="simple-project3-api-document",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="oasisc1208@icloud.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
 )
 
 
-if settings.DEBUG:      ## settings가 True가 되어 있는 경우에만 Swagger용 URL 등록
+if settings.DEBUG:  ## settings가 True가 되어 있는 경우에만 Swagger용 URL 등록
     urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^doc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        re_path(
+            r"^swagger(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^swagger/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            r"^doc/$",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
     ]
-
